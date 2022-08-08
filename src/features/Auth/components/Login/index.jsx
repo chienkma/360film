3ad5b42cbe18a32c4207574618dde7060e29eec3
chienkma <chienmtmdbg@gmail.com>
2022-08-08 17:16:42 +0700

    update code

--- src/features/Auth/components/Login/index.jsx
+++ src/features/Auth/components/Login/index.jsx
@@ -1,27 +1,36 @@
-import React from "react";
-import PropTypes from "prop-types";
 import LoginForm from "../LoginForm";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { getDatabase, ref, update } from "firebase/database";
+import { auth, app } from "../../../../firebase/config";
+import { useSnackbar } from "notistack";
 Login.propTypes = {};
-const handleSubmit = (values) => {
-  try {
-    const email = values.email;
-    const password = values.password;
-    createUserWithEmailAndPassword(auth, email, password).then(
-      (userCredential) => {
-        const user = userCredential.user;
-      }
-    );
-    enqueueSnackbar("Register secessfully", { variant: "success" });
-  } catch (error) {
-    const errorCode = error.code;
-    const errorMessage = error.message;
-    enqueueSnackbar("Register Fail", { variant: "error" });
-  }
-};
+
 function Login(props) {
+  const db = getDatabase(app);
+  const { enqueueSnackbar } = useSnackbar();
+  const handleSubmitLogIn = (values) => {
+    try {
+      const email = values.email;
+      const password = values.password;
+      signInWithEmailAndPassword(auth, email, password).then(
+        (userCredential) => {
+          const user = userCredential.user;
+          let lgDay = new Date();
+          update(ref(db, "users/" + user.uid), {
+            last__lg: lgDay,
+          });
+          enqueueSnackbar("Sign In secessfully", { variant: "success" });
+        }
+      );
+    } catch (error) {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      enqueueSnackbar("Sign In Fail", errorMessage, { variant: "error" });
+    }
+  };
   return (
     <div>
-      <LoginForm onSubmit={handleSubmit} />
+      <LoginForm onSubmit={handleSubmitLogIn} />
     </div>
   );
 }
