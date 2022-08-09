49e2032a6cb542a93c3613990a682a25a7ba79b2
chienkma <chienmtmdbg@gmail.com>
2022-08-09 17:25:28 +0700

    update Codev2

--- src/features/Auth/components/Login/index.jsx
+++ src/features/Auth/components/Login/index.jsx
@@ -1,8 +1,14 @@
-import LoginForm from "../LoginForm";
-import { signInWithEmailAndPassword } from "firebase/auth";
+import { Button, ButtonGroup, Typography } from "@material-ui/core";
+import {
+  FacebookAuthProvider,
+  GoogleAuthProvider,
+  signInWithEmailAndPassword,
+  signInWithPopup,
+} from "firebase/auth";
 import { getDatabase, ref, update } from "firebase/database";
-import { auth, app } from "../../../../firebase/config";
 import { useSnackbar } from "notistack";
+import { app, auth } from "../../../../firebase/config";
+import LoginForm from "../LoginForm";
 Login.propTypes = {};
 
 function Login(props) {
@@ -28,9 +34,54 @@ function Login(props) {
       enqueueSnackbar("Sign In Fail", errorMessage, { variant: "error" });
     }
   };
+  const handleFb = async () => {
+    const provider = new FacebookAuthProvider();
+    await signInWithPopup(auth, provider)
+      .then((result) => {
+        const user = result.user;
+        console.log("User Infor:", user);
+        const credential = FacebookAuthProvider.credentialFromResult(result);
+        const accessToken = credential.accessToken;
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        if (errorCode === "auth/account-exists-with-different-credential") {
+          alert(
+            "You have already signed up with a different auth provider for that email."
+          );
+        } else {
+          console.error(error);
+        }
+        const email = error.customData.email;
+        const credential = FacebookAuthProvider.credentialFromError(error);
+      });
+  };
+  const handleGg = async () => {
+    const provider = new GoogleAuthProvider();
+    await signInWithPopup(auth, provider)
+      .then((result) => {
+        const credential = GoogleAuthProvider.credentialFromResult(result);
+        const token = credential.accessToken;
+
+        const user = result.user;
+        console.log("User Infor:", user);
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        const email = error.customData.email;
+        const credential = GoogleAuthProvider.credentialFromError(error);
+      });
+  };
   return (
     <div>
       <LoginForm onSubmit={handleSubmitLogIn} />
+      <Typography>Or Sign In by</Typography>
+      <ButtonGroup color='primary' aria-label='outlined primary button group'>
+        <Button onClick={handleFb}>FaceBook</Button>
+        <Button onClick={handleGg}>Google</Button>
+      </ButtonGroup>
     </div>
   );
 }
--- src/features/Auth/components/Register/index.jsx
+++ src/features/Auth/components/Register/index.jsx
@@ -5,6 +5,8 @@ import PropTypes from "prop-types";
 import {
   createUserWithEmailAndPassword,
   FacebookAuthProvider,
+  signInWithPopup,
+  GoogleAuthProvider,
 } from "firebase/auth";
 import { auth, app } from "../../../../firebase/config";
 import { getDatabase, ref, set } from "firebase/database";
@@ -16,7 +18,7 @@ function Register(props) {
   const { enqueueSnackbar } = useSnackbar();
   // Register by email
   const db = getDatabase(app);
-  const handleSubmit = async (values) => {
+  const handleSubmitEmail = async (values) => {
     try {
       const username = values.fullName;
       const email = values.email;
@@ -38,13 +40,64 @@ function Register(props) {
       enqueueSnackbar("Register Fail", { variant: "error" });
     }
   };
+  const handleFb = async () => {
+    const provider = new FacebookAuthProvider();
+    await signInWithPopup(auth, provider)
+      .then((result) => {
+        const user = result.user;
+        console.log("User Infor:", user);
+        set(ref(db, "users/" + user.uid), {
+          username: username,
+          email: email,
+        });
+        const credential = FacebookAuthProvider.credentialFromResult(result);
+        const accessToken = credential.accessToken;
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        if (errorCode === "auth/account-exists-with-different-credential") {
+          alert(
+            "You have already signed up with a different auth provider for that email."
+          );
+        } else {
+          console.error(error);
+        }
+        const email = error.customData.email;
+        const credential = FacebookAuthProvider.credentialFromError(error);
+
+        // ...
+      });
+  };
+  const handleGg = async () => {
+    const provider = new GoogleAuthProvider();
+    await signInWithPopup(auth, provider)
+      .then((result) => {
+        const credential = GoogleAuthProvider.credentialFromResult(result);
+        const token = credential.accessToken;
+
+        const user = result.user;
+        console.log("User Infor:", user);
+        set(ref(db, "users/" + user.uid), {
+          username: username,
+          email: email,
+        });
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        const email = error.customData.email;
+        const credential = GoogleAuthProvider.credentialFromError(error);
+        // ...
+      });
+  };
   return (
     <div>
-      <RegisterForm onSubmit={handleSubmit} />
+      <RegisterForm onSubmit={handleSubmitEmail} />
       <Typography>Or Sign In by</Typography>
       <ButtonGroup color='primary' aria-label='outlined primary button group'>
-        <Button>FaceBook</Button>
-        <Button>Google</Button>
+        <Button onClick={handleFb}>FaceBook</Button>
+        <Button onClick={handleGg}>Google</Button>
       </ButtonGroup>
     </div>
   );
